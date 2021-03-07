7dba1c7048cff903b0ce1dbc332b653d0dd82cc2
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-07 23:40:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-    planeteer_calls.map do |element| element.capitalize
+    planeteer_calls.capitalize do |element|
   end
     planeteer_calls.chain do |element| element.concat "!"
 end
