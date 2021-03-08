1ecaa94aad37488d0b7f5d4cf3704d1611aab65d
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-08 00:21:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,4 +18,3 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find. do |element| valid_calls.any? == element 
   end
-end
